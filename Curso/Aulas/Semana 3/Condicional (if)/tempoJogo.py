1114b9000b55807e9ba68c8e662b9af53ec7a931
Juninho Gomes <62433587+juninhogomes@users.noreply.github.com>
2021-02-24 20:25:23 -0300

    Atividade da bliblioteca Math e if

---
+++ Curso/Aulas/Semana 3/Condicional (if)/tempoJogo.py
@@ -0,0 +1,9 @@
+# Programa simples pra saber se o jogo acabou ou não
+tempoJogo = int(input('Quantos minutos de jogo já foram jogados? '))
+
+if tempoJogo <= 90:
+    print('Calma, tranquilidade!')
+    print('Enquanto tem bambu, tem flecha. O jogo só acaba quando termina.')
+
+else:
+    print('AI MEU DEUS! ACABA JUIZÃO! Ou não. Depende.')
