eb068225a130b4032858d1cab0aaf38e3be97f50
Juninho Gomes <62433587+juninhogomes@users.noreply.github.com>
2021-03-02 14:50:57 -0300

    Atividades avaliativas semana 4

---
+++ Curso/Exercícios Avaliativos/Avaliação - Semana 4/n_impares.py
@@ -0,0 +1,11 @@
+n = int(input('Digite o valor de n: '))
+x = 0
+impar = -1
+
+while (x < n):
+    impar += 2
+    x += 1
+    print(impar)
+
+
+
---
+++ Curso/Exercícios Avaliativos/Avaliação - Semana 4/somaDigitos.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um número inteiro: '))
+soma = 0
+
+while (n > 0):
+    soma += n % 10
+    n = n // 10
+
+
+print(soma)
