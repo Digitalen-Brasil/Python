fc2844a0b611dd0f55a8b79f56789553adc6267e
Juninho Gomes <62433587+juninhogomes@users.noreply.github.com>
2021-04-09 12:18:40 -0300

    Soma de números de uma lista

---
+++ Curso/Exercícios Avaliativos/Avaliação - Semana 8/soma_elementos.py
@@ -0,0 +1,7 @@
+def soma_elementos(lista):
+    num = 0 # Numero para iniciar a contagem
+
+    for i in lista:
+        # Vai somar o número (num) padrão mais o i da lista.
+        num += i
+    return num
